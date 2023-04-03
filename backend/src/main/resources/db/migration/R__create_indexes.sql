a1b084a33fb847ab67e0ad7438602f2d8e01cabb
Danil Kuznetsov <skillvrn@yandex.ru>
2023-04-03 11:30:40 +0300

    add index existing in flyway script

--- backend/src/main/resources/db/migration/R__create_indexes.sql
+++ backend/src/main/resources/db/migration/R__create_indexes.sql
@@ -1,7 +1,3 @@
-drop index if exists orders_id_index;
-drop index if exists order_product_index;
-drop index if exists product_id;
-
-CREATE INDEX orders_id_index ON orders (id);
-CREATE INDEX order_product_index ON order_product (order_id, product_id);
-CREATE INDEX product_id ON product (id);
+CREATE INDEX IF NOT EXISTS orders_id_index ON orders (id);
+CREATE INDEX IF NOT EXISTS order_product_index ON order_product (order_id, product_id);
+CREATE INDEX IF NOT EXISTS product_id ON product (id);
