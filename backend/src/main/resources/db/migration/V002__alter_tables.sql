02c6763d81369946b422fb376f56fe78d4c93647
Danil Kuznetsov <skillvrn@yandex.ru>
2023-04-03 11:38:32 +0300

    add BP syntax to flyway scripts

--- backend/src/main/resources/db/migration/V002__alter_tables.sql
+++ backend/src/main/resources/db/migration/V002__alter_tables.sql
@@ -1,3 +1,2 @@
-alter table product alter column name type varchar(20);
-
-alter table product alter column picture_url type varchar(100);
+ALTER TABLE product ALTER COLUMN name TYPE varchar(20);
+ALTER TABLE product ALTER COLUMN picture_url TYPE varchar(100);
